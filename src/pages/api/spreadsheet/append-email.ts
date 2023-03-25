f12977aded359edd0ab96f4749231d1f56e5dec0
Ren Burnett <ren.burnett@mobyinc.com>
2023-03-24 18:00:07 -0700

    wrapped in try/catch

--- src/pages/api/spreadsheet/append-email.ts
+++ src/pages/api/spreadsheet/append-email.ts
@@ -1,11 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-export default async function handler (req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const SHEET_ID = '1t3x3e7z71e5fquiF9sfKrXqeFwbQpF24Z4HeKWz9GY0';
   const doc = new GoogleSpreadsheet(SHEET_ID);
-
-  // const stringCreds = process.env.GOOGLE_CREDS;
 
   const googleEmail = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
   const googleKey = process.env.GOOGLE_PRIVATE_KEY;
@@ -27,39 +25,26 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     return;
   }
 
-  await doc.useServiceAccountAuth({
-    client_email: googleEmail,
-    private_key: googleKey,
-  });
+  try {
+    await doc.useServiceAccountAuth({
+      client_email: googleEmail,
+      private_key: googleKey,
+    });
 
-  // await doc.useServiceAccountAuth(JSON.parse(stringCreds));
+    await doc.loadInfo();
+    console.log(doc.title);
 
-  await doc.loadInfo();
-  console.log(doc.title);
+    const sheet = doc.sheetsByIndex[0];
+    console.log("sheet title!!", sheet.title);
 
-  const sheet = doc.sheetsByIndex[0];
-  console.log("sheet title!!", sheet.title);
+    const { email } = req.query;
 
-  const { email } = req.query;
+    await sheet.setHeaderRow(['email addresses']);
+    await sheet.addRow([email], { insert: false, raw: true });
 
-  let writeSuccess = true;
-  let error = '';
-
-  sheet.setHeaderRow(['email addresses']).then((response: any) => {
-    sheet.addRow([ email ], { insert: false, raw: true }).then((ting:any) => {
-      console.log('ting', ting);
-    }).catch((err: any) => {
-      console.log('error', err);
-
-      error = err;
-      writeSuccess = false;
-    })
-  });
-
-  if (!writeSuccess) {
-    return res.status(500).json({ error: 'Failed to append email', details: error });
-  } else {
     return res.status(200).json({ email });
+  } catch (error) {
+    console.log('error', error);
+    return res.status(500).json({ error: 'Failed to append email', details: error });
   }
-
 }
