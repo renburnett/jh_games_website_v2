a1aad112cc2858c30881518e35f3384db1d96269
Ren Burnett <ren.burnett@mobyinc.com>
2023-05-07 12:08:05 -0700

    more tweaks

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -2,12 +2,16 @@ import Image from 'next/image';
 import NewsletterForm from './components/NewsletterForm';
 import { AppConfig } from '@/utils/AppConfig';
 import mainLogo from '@/images/logo_main_white.png';
+import Head from 'next/head';
 
 type IMainProps = any;
 
 const Main = (props: IMainProps) => {
   return (
     <>
+      <Head>
+        <title>James Hutt Games</title>
+      </Head>
       <div className="grid h-screen place-items-center bg-gray-800 text-white">
         <main className="content py-5 text-xl">
           <Image className="w-56 mb-4 mx-auto" src={mainLogo} alt="James Hutt Games" />
